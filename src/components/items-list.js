22cb263a07a1e420139c019716820f15c8734afd
OUghTMS <misha.saan@gmail.com>
2019-09-05 10:08:41 +0300

    add copy filter

--- src/components/items-list.js
+++ src/components/items-list.js
@@ -4,15 +4,16 @@ import Item from './item';
 
 export default class ItemsList extends Component {
     render() {
-        const { removeItem, editItem } = this.props;
-        const items = this.props.items.map(item => 
-            <Item key={item._id} 
-                  item={item} 
-                  removeItem={removeItem}
-                  editItem={editItem}/>)
+        const { removeItem, editItem, items } = this.props;
+        const newItems = items
+            .filter(( item, index ) => items.indexOf(items.find(copyOfItem => copyOfItem._id === item._id)) === index)
+                .map(item => <Item key={item._id} 
+                                item={item} 
+                                removeItem={removeItem}
+                                editItem={editItem}/>);            
         return (
             <div className="list">
-                {items}
+                {newItems}
             </div>
         );
     }
